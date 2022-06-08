002dcc9350cdd9216762d239969c0de543260ab0
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2022-06-08 09:34:57 +0300

    random theme

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,50 @@
+<script context="module" lang="ts">
+  const themes = [
+    "cerulean",
+    "cosmo",
+    "cyborg",
+    "darkly",
+    "flatly",
+    "journal",
+    "litera",
+    "lumen",
+    "lux",
+    "materia",
+    "minty",
+    "morph",
+    "pulse",
+    "quartz",
+    "sandstone",
+    "simplex",
+    "sketchy",
+    "slate",
+    "solar",
+    "spacelab",
+    "superhero",
+    "united",
+    "vapor",
+    "yeti",
+    "zephyr",
+  ];
+  const theme = themes[~~(Math.random() * themes.length)];
+</script>
+
 <script lang="ts">
-  import "./assets/bootswatch/quartz.min.css";
+  // import "./assets/bootswatch/quartz.min.css";
   import "bootstrap-icons/font/bootstrap-icons.css";
   import "./styles/styles.css";
 
   import { Header, Body, Footer } from "./lib/layout";
-
 </script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="/bootswatch/{theme}.min.css"
+    type="text/css"
+    media="screen"
+  />
+</svelte:head>
 
 <main class="d-flex flex-column min-vh-100">
   <Header />
